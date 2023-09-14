7e71319a16aaae6f636b8e87ca55bc6f00f6c3a5
M-Rahul1 <rahul2001ramesh@gmail.com>
2023-09-14 21:23:22 +0200

    Initial commit

---
+++ components/Nav.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
